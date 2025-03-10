1c7230a17e3b34faa03142832683e069834980e8
Michael Boateng <michaelboateng064@gmail.com>
2025-03-10 22:35:21 +0000

    Accesed props by destructring and using the spread operator

--- src/index.js
+++ src/index.js
@@ -4,33 +4,45 @@ import ReactDOM from "react-dom/client";
 // CSS
 import "./index.css";
 
+// Variables
+const books = [
+  {
+    id: 1,
+    image: "https://picsum.photos/200/100",
+    alt: "Book cover",
+    title: "The Way to The Jungle.",
+    author: "Michael Boateng",
+  },
+  {
+    id: 2,
+    image: "https://picsum.photos/200/100",
+    alt: "Book cover",
+    title: "The Way to Music",
+    author: "Blue Ivy Carter",
+  },
+];
+
 function Booklist() {
   return (
     <section className="book-list">
-      <Book
-        image="https://picsum.photos/200/100"
-        alt="Book cover"
-        title="The Way to The Jungle."
-        author="Michael Boateng"
-      />
-      <Book
-        image="https://picsum.photos/200/100"
-        alt="Book cover"
-        title="The Way to The Jungle."
-        author="Blue Ivy Carter"
-      />
+      {books.map((book, index) => {
+        // const { image, alt, title, author } = book;
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
+  console.log(props);
+  const { title, image, author, alt } = props;
   return (
     <article className="book">
       <div className="wrapper">
-        <img src={props.image} alt={props.alt} width={200} height={100} />
+        <img src={image} alt={alt} width={200} height={100} />
       </div>
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
